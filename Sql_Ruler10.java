c99f41e9a619e242f91cfef3c010b5fb8c5ff5ea
Maciej Wadas <53495687+MaciejITT@users.noreply.github.com>
2020-11-16 19:32:59 +0100

    Update Sql_Ruler10.java
    3

--- Sql_Ruler10.java
+++ Sql_Ruler10.java
@@ -109,7 +109,7 @@ class GetConnection {
         CONNECTION = "jdbc:mysql://10.0.10.3:3306/fspwcho_baza";
     
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(CONNECTION, USERNAME, PASSWORD);
             return conn;
         }catch(SQLException | ClassNotFoundException e){
